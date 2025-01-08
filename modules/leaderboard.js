a04c144770666ba30e66dde590e36ea0bf93cc48
mee-i <ilhamhizam9@gmail.com>
2025-01-08 10:48:53 +0700

    Small changes

--- modules/leaderboard.js
+++ modules/leaderboard.js
@@ -7,9 +7,10 @@ module.exports = {
         const leaderboard = [];
         for (let i = 0; i < 10; i++) {
             const user = data[sorted[i]];
-            leaderboard.push(`${i + 1}. ${user.name} - Level ${user.level} - XP ${user.xp}`);
+            leaderboard.push(`${i + 1}. *${user.name}* - Level *${user.level}* - XP *${user.xp}*`);
         }
-        await sock.sendMessage(msg?.key?.remoteJid, { text: leaderboard.join("\n") }, { quoted: msg });
+        await sock.sendMessage(msg?.key?.remoteJid, { text: `*Leaderboard MeeI Bot*
+            ${leaderboard}` }, { quoted: msg });
     },
     Config: {
         menu: "Leaderboard"
