f64cc0828eab9fdc92582a357ba5bfcda9b3300d
mee-i <ilhamhizam9@gmail.com>
2025-01-08 10:44:26 +0700

    Add /leaderboard

---
+++ modules/leaderboard.js
@@ -0,0 +1,17 @@
+const db = require('../utilities/database');
+
+module.exports = {
+    leaderboard: async (sock, msg) => {
+        const data = await db.UserData.ReadUserData();
+        const sorted = Object.keys(data).sort((a, b) => data[b].xp - data[a].xp);
+        const leaderboard = [];
+        for (let i = 0; i < 10; i++) {
+            const user = data[sorted[i]];
+            leaderboard.push(`${i + 1}. ${user.name} - Level ${user.level} - XP ${user.xp}`);
+        }
+        await sock.sendMessage(msg?.key?.remoteJid, { text: leaderboard.join("\n") }, { quoted: msg });
+    },
+    Config: {
+        menu: "Leaderboard"
+    }
+}
--- utilities/database.js
+++ utilities/database.js
@@ -53,4 +53,5 @@ module.exports = {
       WriteUserData({});
     },
   },
+  ReadUserData
 };
