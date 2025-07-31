2c533a947b41ba2b3acfe0abc4b7eeb25ce69c86
mee-i <ilhamhizam9@gmail.com>
2025-07-31 21:47:24 +0700

    refactor: code format feat(db): money and daily_time

--- database/schema.js
+++ database/schema.js
@@ -1,45 +1,69 @@
-import { mysqlTable, varchar, text, index, unique, int, boolean, timestamp, float } from 'drizzle-orm/mysql-core';
+import {
+  mysqlTable,
+  varchar,
+  text,
+  index,
+  unique,
+  int,
+  boolean,
+  timestamp,
+  float,
+  bigint,
+} from "drizzle-orm/mysql-core";
 
-export const authTable = mysqlTable('auth', {
-    session: varchar('session', { length: 50 }).notNull(),
-    id: varchar('id', { length: 100 }).notNull(),
-    value: text('value'),
-}, (auth) => [
-    unique('idxunique').on(auth.session, auth.id),
-    index('idxsession').on(auth.session),
-    index('idxid').on(auth.id),
-]);
+export const authTable = mysqlTable(
+  "auth",
+  {
+    session: varchar("session", { length: 50 }).notNull(),
+    id: varchar("id", { length: 100 }).notNull(),
+    value: text("value"),
+  },
+  (auth) => [
+    unique("idxunique").on(auth.session, auth.id),
+    index("idxsession").on(auth.session),
+    index("idxid").on(auth.id),
+  ]
+);
 
-export const userTable = mysqlTable('user', {
-    id: varchar('id', { length: 100 }).notNull().primaryKey(),
-    name: varchar('name', { length: 100 }).notNull(),
-    xp: int('xp').notNull().default(0),
-    level: int('level').notNull().default(0),
-    premium: boolean('premium').notNull().default(false),
+export const userTable = mysqlTable("user", {
+  id: varchar("id", { length: 100 }).notNull().primaryKey(),
+  name: varchar("name", { length: 100 }).notNull(),
+  xp: int("xp").notNull().default(0),
+  level: int("level").notNull().default(0),
+  premium: boolean("premium").notNull().default(false),
+  money: bigint("money", { mode: "number" }).notNull().default(0),
+  daily_time: timestamp().defaultNow().notNull(),
 });
 
-export const messageNotificationTable = mysqlTable('message_notification', {
-    id: varchar('id', { length: 100 }).notNull().primaryKey(),
-    lat: float('lat').notNull(),
-    lon: float('lon').notNull(),
+export const storeTable = mysqlTable("store", {
+  id: varchar("id", { length: 100 }).notNull().primaryKey(),
+  name: varchar("name", { length: 100 }).notNull(),
+  price: bigint("price", { mode: "number" }).notNull(),
+  image: varchar("image", { length: 100 }),
+  description: text("description"),
+})
+export const messageNotificationTable = mysqlTable("message_notification", {
+  id: varchar("id", { length: 100 }).notNull().primaryKey(),
+  lat: float("lat").notNull(),
+  lon: float("lon").notNull(),
 });
 
-export const earthquakeTable = mysqlTable('earthquake', {
-    event_id: varchar('event_id', { length: 100 }).notNull().primaryKey(),
-    status: varchar('status', { length: 20 }).notNull(),
-    waktu: timestamp('waktu', { mode: 'string' }).notNull(),
-    lintang: float('lintang').notNull(),
-    bujur: float('bujur').notNull(),
-    dalam: int('dalam').notNull(),
-    mag: float('mag').notNull(),
-    fokal: varchar('fokal', { length: 50 }).notNull(),
-    area: varchar('area', { length: 100 }).notNull(),
+export const earthquakeTable = mysqlTable("earthquake", {
+  event_id: varchar("event_id", { length: 100 }).notNull().primaryKey(),
+  status: varchar("status", { length: 20 }).notNull(),
+  waktu: timestamp("waktu", { mode: "string" }).notNull(),
+  lintang: float("lintang").notNull(),
+  bujur: float("bujur").notNull(),
+  dalam: int("dalam").notNull(),
+  mag: float("mag").notNull(),
+  fokal: varchar("fokal", { length: 50 }).notNull(),
+  area: varchar("area", { length: 100 }).notNull(),
 });
 
-export const commandLogTable = mysqlTable('command_log', {
-    id: varchar('id', { length: 100 }).notNull(),
-    command: varchar('command', { length: 100 }).notNull(),
-    args: text('args').notNull(),
-    timestamp: timestamp('timestamp', { mode: 'string' }).notNull().defaultNow(),
-    isGroup: boolean('isGroup').notNull().default(false),
+export const commandLogTable = mysqlTable("command_log", {
+  id: varchar("id", { length: 100 }).notNull(),
+  command: varchar("command", { length: 100 }).notNull(),
+  args: text("args").notNull(),
+  timestamp: timestamp("timestamp", { mode: "string" }).notNull().defaultNow(),
+  isGroup: boolean("isGroup").notNull().default(false),
 });
